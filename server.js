d1fb808b3cc8d36fec8d080dd65bcc1f0a391364
unknown <msk-tkonovalov@tech08.int.maxmin.ru>
2015-10-30 22:13:07 +0300

    routing

--- server.js
+++ server.js
@@ -4,9 +4,22 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function(req, res) {
 	var body = "<html><body><h1 style='color: red'>Hello world</h1></body></html>"
-	res.writeHead(200, {
-		'Content-Length': body.length,
-		'Content-Type': 'text/html' 
-	});
-	res.end(body);
+	
+	switch (req.url) {
+		case "/": 	
+			res.writeHead(200, {
+				'Content-Length': body.length,
+				'Content-Type': 'text/html' 
+			});
+			res.end(body);
+			break;
+		case "/app":
+			res.writeHead(200, {'content-Type': 'text/plain'});
+			res.end("раз два раз");
+			break;
+		default: 
+			res.writeHead(404);
+			break;
+	}
+
 }).listen(port);
