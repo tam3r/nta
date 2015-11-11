01175543e355105d31af234c0d3a3f10e50bf8bf
unknown <msk-tkonovalov@tech08.int.maxmin.ru>
2015-11-12 00:06:34 +0300

    fix pararms

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var KTGetXMLData = require('./utils/yaFeed');
+var url = require('url');
 
 var port = process.env.PORT || 1337;
 
@@ -11,8 +12,10 @@ setInterval(function() {yandexSportFeed.getData(); console.log("renewed")}, 5000
 
 http.createServer(function(req, res) {
 	var body = "<html><body><h1 style='color: red'>Hello world</h1></body></html>"
+	pathname = url.parse(req.url, true).pathname;
+	wrapper = url.parse(req.url, true).query.callback
 	
-	switch (req.url) {
+	switch (pathname) {
 		case "/": 	
 			res.writeHead(200, {
 				'Content-Length': body.length,
@@ -26,7 +29,8 @@ http.createServer(function(req, res) {
 			break;
 		case "/app/feed":
 			res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-			res.end("{\"data\":" + JSON.stringify(yandexSportFeed.data) + "}");
+			resString = wrapper ? wrapper + "({\"data\":" + JSON.stringify(yandexSportFeed.data) + "})" : "{\"data\":" + JSON.stringify(yandexSportFeed.data) + "}";
+			res.end(resString);
 			break;
 		default: 
 			res.writeHead(404, {'content-Type': 'text/html'});
