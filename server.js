5677aec6beac4e483afe09fe1c7e6eb043bc3ccb
unknown <timur.konovalov@gmail.com>
2015-11-11 14:05:30 +0300

    final

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var port = process.env.PORT || 1337;
 
 var parseString = require('xml2js').parseString;
 
+var request = require('request');
+
 http.createServer(function(req, res) {
 	var body = "<html><body><h1 style='color: red'>Hello world</h1></body></html>"
 	
@@ -19,12 +21,22 @@ http.createServer(function(req, res) {
 			res.writeHead(200, {'content-Type': 'text/plain'});
 			res.end("раз два раз");
 			break;
-		default: 
-			res.writeHead(200, {'content-Type': 'application/json'});
-			var xml = "<root>Hello xml2js!</root>"
-			parseString(xml, function (err, result) {
-				res.end(JSON.stringify(result));
+		case "/app/feed":
+			request.get('http://news.yandex.ru/sport.rss', function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+					res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+					parseString(body, {explicitArray: false, ignoreAttrs: true}, 
+						function (err, result) {
+							outJSON = JSON.stringify({"data": result.rss.channel.item[0]});
+							res.end(outJSON);
+						}
+					);
+				}
 			});
+			break;
+		default: 
+			res.writeHead(404, {'content-Type': 'text/html'});
+			res.end();
 			break;
 	}
 
