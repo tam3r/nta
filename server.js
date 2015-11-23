4830a1e344f491a20da5f8014c36cd8d1b64413f
unknown <msk-tkonovalov@tech08.int.maxmin.ru>
2015-11-23 15:29:01 +0300

    express

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var KTGetXMLData = require('./utils/yaFeed');
 var url = require('url');
+var app = require('express')();
+
 
 var port = process.env.PORT || 1337;
 
@@ -10,33 +12,39 @@ console.log("server started");
 console.log(yandexSportFeed);
 setInterval(function() {yandexSportFeed.getData(); console.log("renewed")}, 10*60*1000)
 
-http.createServer(function(req, res) {
-	var body = "<html><body><h1 style='color: red'>Hello world</h1></body></html>"
-	pathname = url.parse(req.url, true).pathname;
-	wrapper = url.parse(req.url, true).query.callback
-	
-	switch (pathname) {
-		case "/": 	
-			res.writeHead(200, {
-				'Content-Length': body.length,
-				'Content-Type': 'text/html' 
-			});
-			res.end(body);
-			break;
-		case "/app":
-			res.writeHead(200, {'content-Type': 'text/plain'});
-			res.end("раз два раз");
-			break;
-		case "/app/feed":
-			res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-			resString = wrapper ? wrapper + "({\"data\":" + JSON.stringify(yandexSportFeed.data) + "})" 
-								: "{\"data\":" + JSON.stringify(yandexSportFeed.data) + "}";
-			res.end(resString);
-			break;
-		default: 
-			res.writeHead(404, {'content-Type': 'text/html'});
-			res.end();
-			break;
-	}
+app.get('/app/feed', function ya_sport(req, res) {
+	res.jsonp(yandexSportFeed.data);
+})
 
-}).listen(port);
+app.listen(port);
+
+// http.createServer(function(req, res) {
+// 	var body = "<html><body><h1 style='color: red'>Hello world</h1></body></html>"
+// 	pathname = url.parse(req.url, true).pathname;
+// 	wrapper = url.parse(req.url, true).query.callback
+	
+// 	switch (pathname) {
+// 		case "/": 	
+// 			res.writeHead(200, {
+// 				'Content-Length': body.length,
+// 				'Content-Type': 'text/html' 
+// 			});
+// 			res.end(body);
+// 			break;
+// 		case "/app":
+// 			res.writeHead(200, {'content-Type': 'text/plain'});
+// 			res.end("раз два раз");
+// 			break;
+// 		case "/app/feed":
+// 			res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+// 			resString = wrapper ? wrapper + "({\"data\":" + JSON.stringify(yandexSportFeed.data) + "})" 
+// 								: "{\"data\":" + JSON.stringify(yandexSportFeed.data) + "}";
+// 			res.end(resString);
+// 			break;
+// 		default: 
+// 			res.writeHead(404, {'content-Type': 'text/html'});
+// 			res.end();
+// 			break;
+// 	}
+
+// }).listen(port);
