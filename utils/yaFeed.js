114d8b726764bbb020099e99de6beffa842ca901
unknown <msk-tkonovalov@tech08.int.maxmin.ru>
2015-11-25 14:31:04 +0300

    time fix 2

--- utils/yaFeed.js
+++ utils/yaFeed.js
@@ -2,10 +2,6 @@ var parseString = require('xml2js').parseString;
 
 var request = require('request');
 
-var monthE2R = [
-  'января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа',
-  'сентября', 'октября', 'ноября', 'декабря'];
-
 function compareDates(a, b) {
     var first = new Date(a.pubDate);
     var second = new Date(b.pubDate)
@@ -25,28 +21,20 @@ KTGetXMLData.prototype.getData = function(callback) {
 
 function processData(obj, callback) {
     request.get(obj.source, function (error, response, body) {
+        
         if (!error && response.statusCode == 200) {
+            
             parseString(body, {explicitArray: false, ignoreAttrs: true}, 
                 function(err, result) {
+                    
                     obj.data = result.rss.channel.item;
                     obj.data.sort(compareDates);
-                    var currentDate = "";
                     obj.data.forEach(function normalise(item) {
                         var dateSrc = new Date(item.pubDate);
-                        item.pubDateStr = dateSrc.getDate() + ' ' + monthE2R[dateSrc.getMonth()];
-                        var minutes = '0' + dateSrc.getMinutes();
-                        item.pubTimeStr = dateSrc.getHours() + ':' + minutes.slice(-2);
-                
-                        if (currentDate !== item.pubDateStr) {
-                            currentDate = item.pubDateStr;
-                            item.newDate = "true";
-                        } else {
-                            item.newDate = "false";
-                        } 
+                        item.pubDate = dateSrc.getTime();
                         
                         delete item.guid;
                         delete item.link;
-                        delete item.pubDate;
                     })
                 }
             );
